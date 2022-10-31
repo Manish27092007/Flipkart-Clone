7e9387be1cba53b1ea8a1e553fca714134924fa5
D!kshant★ <50083135+DikshantArunSable@users.noreply.github.com>
2022-10-31 15:37:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var load= document.getElementById("preloader");
+
+function loadfun()
+{
+load.style.display = 'none';
+}
+
+const header = document.querySelector('.Header');
+const nav = document.querySelector('.Navbar')
+const topOfNav = header.offsetTop;
+
+function fixNav(){
+    if (window.scrollY >= topOfNav){
+        nav.style.paddingTop = header.offsetHeight + 2 + 'px';
+    }
+    else{
+        nav.style.paddingTop = 0;
+    }
+}
+// for fixing the navbar at top
+window.addEventListener('scroll',fixNav);
